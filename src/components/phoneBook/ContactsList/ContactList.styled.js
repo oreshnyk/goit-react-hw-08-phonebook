c9c698db99f13d716c8225113f09ccf5442bc1e5
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2022-12-17 03:54:56 +0100

    finished registration

---
+++ src/components/phoneBook/ContactsList/ContactList.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { ListItem } from '@mui/material';
+
+export const Item = styled(ListItem)`
+  :hover {
+    background-color: #ffd700;
+    border-radius: 8px;
+  }
+`;
